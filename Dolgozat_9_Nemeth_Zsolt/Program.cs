4993b39ae3a8ab9597ea2ab1cecf430a5a29a2a1
NemethZsolt19 <153748827+NemethZsolt19@users.noreply.github.com>
2024-03-25 13:51:37 +0100

    Add files via upload

---
+++ Dolgozat_9_Nemeth_Zsolt/Program.cs
@@ -0,0 +1,14 @@
+﻿using KonyvAdat.Models;
+using KonyvAdat.Data;
+using Microsoft.EntityFrameworkCore;
+
+string[] sorok = File.ReadAllLines(@"c:\adat\9.csv");
+
+if (!sorok.Any())
+{
+
+    foreach (var sor in sorok)
+    {
+        _context.Adatok.Add(new Adatok(sor));
+    }
+}
